528a21052d9a7f93101abb820ec208c5428d92cc
Michael Habibi <github@mikehabibi.com>
2014-11-29 13:45:55 -0600

    Add make target to make an ISO image, make iso. Requires grub-mkrescue and xorriso

---
+++ src/tools/makeiso.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+os_image=$1
+iso_dir=$2
+
+if [ ! -f $os_image ]; then
+  echo "Can't find file $os_image, aborting"
+  exit 1
+fi
+
+if ! which grub-mkrescue; then
+  echo "ERROR: You need grub-mkrescue installed in order to make an ISO"
+  exit 1
+fi
+
+if ! which xorriso; then
+  echo "ERROR: You need xorriso installed in order to make an ISO"
+  echo "Using Debian? Try sudo apt-get install xorriso"
+  exit 1
+fi
+
+mkdir -p $iso_dir/boot/grub
+cp $os_image $iso_dir/boot
+cat << EOF 1>$iso_dir/boot/grub/grub.cfg
+menuentry "osdev" {
+  multiboot /boot/$os_image
+}
+EOF
+grub-mkrescue -o $iso_dir/cdrom.iso $iso_dir
